a6846e96f360d3f88e4471caab3204b0fe6a498c
Juhász Bálint <JuhaszBalint@pg.local>
2026-03-11 10:45:13 +0100

    Update ketdimenziostombletrehozas.py

--- ketdimenziostombletrehozas.py
+++ ketdimenziostombletrehozas.py
@@ -1,5 +1,14 @@
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
+tomb = []
+
+sor = 4
+oszlop = 3
+szam = 1
+
+for i in range(sor):
+    sor_tomb = []
+    for j in range(oszlop):
+        sor_tomb.append(szam)
+        szam += 1
+    tomb.append(sor_tomb)
+
+print(tomb)
